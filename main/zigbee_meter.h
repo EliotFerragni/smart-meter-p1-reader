2d886f575c36c6d5ed7db4f31710f2feab12ddb1
Eliot Ferragni <IceShadow34@gmail.com>
2024-11-20 20:27:35 +0100

    Fix swapped comments

--- main/zigbee_meter.h
+++ main/zigbee_meter.h
@@ -3,17 +3,18 @@
 /* Zigbee Library */
 #include "esp_zigbee_core.h"
 
-/**
- * @brief Set and send the new power given in parameter
- * 
- * @param power 
- * @return esp_err_t 
- */
-void zb_electricity_meter_ep(esp_zb_ep_list_t *esp_zb_ep_list);
 
 /**
  * @brief Create endpoint for electrical measurement
  * 
  * @param esp_zb_ep_list List used to add the endpoint
+ */
+void zb_electricity_meter_ep(esp_zb_ep_list_t *esp_zb_ep_list);
+
+/**
+ * @brief Set and send the new power given in parameter
+ * 
+ * @param power 
+ * @return esp_err_t 
  */
 esp_err_t zb_update_total_active_power(int32_t power);
